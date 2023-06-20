ea495179140db89303cf0607c90ccf61326feb96
Jose Celano <josecelano@gmail.com>
2023-06-20 08:14:17 +0100

    add timeout layer and http1_header_read_timeout

--- src/app.rs
+++ src/app.rs
@@ -10,9 +10,13 @@ use std::net::SocketAddr;
 use std::time::Duration;
 use tokio::time::sleep;
 use tower::ServiceBuilder;
+use tower_http::timeout::TimeoutLayer;
 
+/// # Panics
+///
+/// It panics if an invalid socket address is provided.
 pub fn run() -> impl Future<Output = hyper::Result<()>> {
-    let config_socket_addr: SocketAddr = "127.0.0.1:3001".parse().unwrap();
+    let config_socket_addr: SocketAddr = "127.0.0.1:3001".parse().expect("a valid socket address");
 
     let tcp_listener = std::net::TcpListener::bind(config_socket_addr)
         .expect("tcp listener to bind to a socket address");
@@ -21,23 +25,25 @@ pub fn run() -> impl Future<Output = hyper::Result<()>> {
         .local_addr()
         .expect("tcp listener to be bound to a socket address.");
 
-    println!("API server listening on http://{}", bound_addr);
+    println!("API server listening on http://{bound_addr}");
 
     let app = Router::new().route("/", get(entrypoint_handler)).layer(
         ServiceBuilder::new()
             .layer(HandleErrorLayer::new(handle_timeout_error))
+            .layer(TimeoutLayer::new(Duration::from_secs(5)))
             .timeout(Duration::from_secs(5)),
     );
 
     let server = axum::Server::from_tcp(tcp_listener)
         .expect("a new server from the previously created tcp listener.")
+        .http1_header_read_timeout(Duration::from_secs(5))
         .serve(app.into_make_service_with_connect_info::<SocketAddr>());
 
     server.with_graceful_shutdown(async move {
         tokio::signal::ctrl_c()
             .await
             .expect("failed to listen to shutdown signal.");
-        println!("Stopping API server on http://{} ...", bound_addr);
+        println!("Stopping API server on http://{bound_addr} ...");
     })
 }
 
@@ -52,6 +58,7 @@ pub async fn entrypoint_handler() -> Response {
         .into_response()
 }
 
+#[allow(clippy::unused_async)]
 async fn handle_timeout_error(
     // `Method` and `Uri` are extractors so they can be used here
     method: Method,
@@ -61,6 +68,6 @@ async fn handle_timeout_error(
 ) -> (StatusCode, String) {
     (
         StatusCode::INTERNAL_SERVER_ERROR,
-        format!("`{} {}` failed with {}", method, uri, err),
+        format!("`{method} {uri}` failed with {err}"),
     )
 }
